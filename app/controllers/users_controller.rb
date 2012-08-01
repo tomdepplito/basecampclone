22c7d7ae676a7e362872f16a2c90848f80617f4e
John Davison + Tom Depplito <apprentice+jd+td@devbootcamp.com>
2012-08-01 11:43:06 -0700

    iteration2 done and merged

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
